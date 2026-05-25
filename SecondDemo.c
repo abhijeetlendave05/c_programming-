8d2cbc092f447e4e804be27c028a1a7691d130b7
abhiii0505 <abhijeetlendave0505@gmail.com>
2026-05-25 11:59:02 +0530

    Programs

---
+++ SecondDemo.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int Add(int, int);
+
+int main()
+{
+    int x = 10;
+    int y = 11;
+    int result;
+    result=Add(x,y);
+    printf("Addition:%d\n",result);
+
+    return 0;
+}
